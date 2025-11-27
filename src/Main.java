b25519c8bc705bd65d8033daf07e6b0b90b121b3
SSZA <angel.sanzam@educa.jcyl.es>
2025-11-27 19:58:07 +0100

    Clase 27/11/25

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import dominio.CPU;
+import dominio.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -17,11 +17,65 @@ public class Main {
         CPU cpu2= new CPU();
         cpu2.setTipo("Intel");
         System.out.println(cpu2.getTipo());
-        //Visualizo los valoresen
+        //Visualizo los valores en
         System.out.println(cpu.getTipo());
         System.out.println(cpu.getGeneracion());
         System.out.println(cpu.getNucleo());
         System.out.println(cpu.getMemCache());
         System.out.println(cpu.getVelocidad());
+
+        //Me creo una memoria en blanco
+        Memoria memoria=new Memoria();
+
+        //Asigno los valores
+        memoria.setTipo("DDR5");
+        memoria.setCapacidad("16GB");
+        memoria.setVelocidad("6400MHz");
+
+        //Imprimo los valores
+        System.out.println(memoria.getTipo());
+        System.out.println(memoria.getCapacidad());
+        System.out.println(memoria.getVelocidad());
+
+        //Me creo una placa base en blanco
+        PlacaBase placaBase=new PlacaBase();
+
+        //Asigno los valores
+        placaBase.setSocket("LGA");
+        placaBase.setPci("Pci x4");
+        placaBase.setMarca("MSI");
+        placaBase.setFactorForma("ATX");
+
+        //Imprimo los valores
+        System.out.println(placaBase.getSocket());
+        System.out.println(placaBase.getPci());
+        System.out.println(placaBase.getMarca());
+        System.out.println(placaBase.getFactorForma());
+
+        //Me creo un almacenamiento en blanco
+        Almacenamiento almacenamiento=new Almacenamiento();
+
+        //Asigno los valores
+        almacenamiento.setMarca("Kingston");
+        almacenamiento.setCapacidad("1 TB");
+        almacenamiento.setConexion("SATA");
+
+        //Imprimo los valores
+        System.out.println(almacenamiento.getMarca());
+        System.out.println(almacenamiento.getCapacidad());
+        System.out.println(almacenamiento.getConexion());
+
+        //Me creo una Fuente de alimentacion en blanco
+        FuenteAlimentacion fuenteAlimentacion=new FuenteAlimentacion();
+
+        //Asigno los valores
+        fuenteAlimentacion.setMarca("Corsair");
+        fuenteAlimentacion.setCertificacion("80 Gold");
+        fuenteAlimentacion.setVoltaje("650 W");
+
+        //Imprimo los valores
+        System.out.println(fuenteAlimentacion.getMarca());
+        System.out.println(fuenteAlimentacion.getCertificacion());
+        System.out.println(fuenteAlimentacion.getVoltaje());
     }
 }
--- src/dominio/PlacaBase.java
+++ src/dominio/PlacaBase.java
@@ -3,6 +3,8 @@ package dominio;
 public class PlacaBase {
     private String socket;
     private String pci;
+    private String marca;
+    private String factorForma;
 
     public String getPci() {
         return pci;
@@ -18,5 +20,21 @@ public class PlacaBase {
 
     public void setSocket(String socket) {
         this.socket = socket;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getFactorForma() {
+        return factorForma;
+    }
+
+    public void setFactorForma(String factorForma) {
+        this.factorForma = factorForma;
     }
 }
